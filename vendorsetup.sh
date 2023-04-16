f1d3c4b119db009107305af0164af45177485e4e
Edison19922001 <123841317+Edison19922001@users.noreply.github.com>
2023-04-15 22:40:27 -0500

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,10 @@
-# Android device tree for motorola moto g22 (hawaiip)
-
-```
 #
 # Copyright (C) 2023 The Android Open Source Project
 # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+add_lunch_combo omni_hawaiip-user
+add_lunch_combo omni_hawaiip-userdebug
+add_lunch_combo omni_hawaiip-eng
